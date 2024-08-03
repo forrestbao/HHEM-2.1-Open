fc51a6a0b0af44785c6785e73d3cab85dcb7431b
forrest bao <forrest@vectara.com>
2024-08-02 22:48:42 -0700

    update message

--- server.py
+++ server.py
@@ -12,20 +12,16 @@ def HHEM2_Web(
         ) -> Markdown:
     """ # The web demo of [Vectara](https://vectara.com)'s [HHEM-2.1-Open](https://huggingface.co/vectara/hallucination_evaluation_model)
 
-    ### Disclaimer
-
-    This app is personally maintained by Vectara lovers on [Github](https://github.com/forrestbao/HHEM-2.1-Open). Opinions expressed here do not reflect those of Vectara. 
+    **Disclaimer**: This app is personally maintained by Vectara lovers on [Github](https://github.com/forrestbao/HHEM-2.1-Open). Opinions expressed here do not reflect those of Vectara. 
 
     
-    ### Usage 
+    **Usage**: Simply enter a premise and a hypothesis and click the Run button. [HHEM-2.1-Open](https://huggingface.co/vectara/hallucination_evaluation_model) will give you a score, between 0 (very hallucinated) and 1 (very consistent), gauging how well the hypothesis is supported by the premise. 
 
-    Simply enter a premise and a hypothesis and [HHEM-2.1-Open](https://huggingface.co/vectara/hallucination_evaluation_model) will give you a score, between 0 (very hallucinated) and 1 (very consistent), gauging how well the hypothesis is supported by the premise. 
-
-    The placeholder example below is hallucinated. Can you tell which part is hallucinated? Hint: Try to remove "and RAG" and see if the score changes.
+    For example, the placeholder example (if you don't see it, refresh your web browser) below is hallucinated. If you remove "and RAG" and re-run, the score will jump much higher. 
 
     """
     pairs = [(premise, hypothesis)]
     if len(premise) + len(hypothesis) > 2000*4: 
         return "Your input is too long. Please ensure that premise and hypothesis is under 2000 characters altogether." 
     scores = model.predict(pairs)   
-    return f"The score is {scores[0]:.2f}."
+    return f"The score is \n # {scores[0]:.2f}."
